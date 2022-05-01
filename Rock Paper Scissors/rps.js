2cb2f7bf0725b674cea4e4ae2e2b6405591be8d3
djballowe <djballowe@gmail.com>
2022-04-30 20:27:20 -0700

    Rock Paper Scissors started

--- Rock Paper Scissors/rps.js
+++ Rock Paper Scissors/rps.js
@@ -1,45 +1,66 @@
 function computerPlay() {
-    let choice = ['paper', 'rock', 'scissors'];
+    let choice = ['paper'];
     let ans = Math.floor(Math.random()*choice.length);
     let value = choice[ans];
     return value;
 }
 
-function playRound(pSelection, cSelection) {
-    if (cSelection === 'paper' && pSelection === 'scissors') {
+function playRound(human, cpu) {
+   
+    if (cpu === 'paper' && human === 'scissors') {
         return 'You Win! Scissors beats paper';
     }
-    if (cSelection === 'rock' && pSelection === 'scissors') {
+    if (cpu === 'rock' && human === 'scissors') {
         return 'You Lose! Rock beats scissors';
     }
-    if (cSelection === 'scissors' && pSelection === 'scissors') {
+    if (cpu === 'scissors' && human === 'scissors') {
         return 'TIE';
     }
-    if (cSelection === 'paper' && pSelection === 'rock') {
+    if (cpu === 'paper' && human === 'rock') {
         return 'You Lose! Paper beats rock';
     }
-    if (cSelection === 'rock' && pSelection === 'rock') {
+    if (cpu === 'rock' && human === 'rock') {
         return 'TIE';
     }
-    if (cSelection === 'scissors' && pSelection === 'rock') {
+    if (cpu === 'scissors' && human === 'rock') {
         return 'You Win! Rock beats scissors';
     } 
-    if (cSelection === 'paper' && pSelection === 'paper') {
+    if (cpu === 'paper' && human === 'paper') {
         return 'TIE';
     }  
-    if (cSelection === 'rock' && pSelection === 'paper') {
+    if (cpu === 'rock' && human === 'paper') {
         return 'You Win! Paper beats rock';
     }
-    if (cSelection === 'scissors' && pSelection === 'paper') {
+    if (cpu === 'scissors' && human === 'paper') {
         return 'You Lose! Scissors beats paper';
     }
 }
 
-function game() {
-    for (let i = 0; i < 5; i++) {
-        playRound()
+function game(human, cpu) {
+    let roundNumber = 0;
+    let humanScore = 0;
+    let computerScore = 0;
+    if (cpu === 'paper' && human === 'rock') {
+        humanScore++;
     }
-    
+    if (cpu === 'scissors' && human === 'rock') {
+        computerScore++;
+    }
+    if (cpu === 'rock' && human === 'paper') {
+        humanScore++;
+    }
+    if (cpu === 'scissors' && human === 'paper') {
+        computerScore++;
+    }
+    if (cpu === 'paper' && human === 'scissors') {
+        humanScore++;
+    }
+    if (cpu === 'rock' && human === 'scissors') {
+        computerScore++;
+    }
+
+    console.log(humanScore)
+
 }
 
 
@@ -47,3 +68,4 @@ const pSelection = 'scissors';
 const cSelection = computerPlay();
 
 console.log(playRound(pSelection, cSelection));
+console.log(game('scissors', 'paper'));
