a8f30431ce75a2d1da2fa3837925b63a5db6385d
djballowe <djballowe@gmail.com>
2022-05-02 07:34:21 -0700

    changing the game function around

--- Rock Paper Scissors/rps.js
+++ Rock Paper Scissors/rps.js
@@ -6,42 +6,59 @@ function computerPlay() {
 }
 
 function playRound(human, cpu) {
-   
+    
     if (cpu === 'paper' && human === 'scissors') {
-        return 'You Win! Scissors beats paper';
+            return 'You Win! Scissors beats paper';
     }
     if (cpu === 'rock' && human === 'scissors') {
-        return 'You Lose! Rock beats scissors';
+            return 'You Lose! Rock beats scissors';
     }
     if (cpu === 'scissors' && human === 'scissors') {
-        return 'TIE';
+            return 'TIE';
     }
     if (cpu === 'paper' && human === 'rock') {
-        return 'You Lose! Paper beats rock';
+            return 'You Lose! Paper beats rock';
     }
     if (cpu === 'rock' && human === 'rock') {
-        return 'TIE';
+            return 'TIE';
     }
     if (cpu === 'scissors' && human === 'rock') {
-        return 'You Win! Rock beats scissors';
+            return 'You Win! Rock beats scissors';
     } 
     if (cpu === 'paper' && human === 'paper') {
-        return 'TIE';
+            return 'TIE';
     }  
     if (cpu === 'rock' && human === 'paper') {
-        return 'You Win! Paper beats rock';
+            return 'You Win! Paper beats rock';
     }
     if (cpu === 'scissors' && human === 'paper') {
-        return 'You Lose! Scissors beats paper';
+            return 'You Lose! Scissors beats paper';
     }
 }
 
+/*function userPrompt() {
+        let userChoice = prompt('Enter your choice');
+        if (userChoice === 'rock' || userChoice === 'paper' || userChoice === 'scissors') {
+                return userChoice;
+        } else {
+                return 'invalid selection';
+        }
+}
+*/
 function game(human, cpu) {
     let roundNumber = 0;
     let humanScore = 0;
     let computerScore = 0;
     for (let i = 0; i < 4; i++) {
-        console.log(playRound(pSelection, cSelection));
+                game();
+                computerPlay();
+                console.log(playRound(pSelection, cSelection));
+                console.log(`Human Score: ${humanScore}`);
+                console.log(`Computer Score: ${computerScore}`);
+                console.log(`Round: ${roundNumber}`);    
+        } 
+        
+    
         if (cpu === 'paper' && human === 'rock') {
             humanScore++;
             Round++;
@@ -59,23 +76,22 @@ function game(human, cpu) {
             roundNumber++;
         }
         if (cpu === 'paper' && human === 'scissors') {
-            humanScore++;
-            roundNumber++;
+                humanScore++;
+                roundNumber++;
         }
         if (cpu === 'rock' && human === 'scissors') {
-            computerScore++;
-            roundNumber++;
+                computerScore++;
+                roundNumber++;
         }
-        
+            
         console.log(`Human Score: ${humanScore}`);
         console.log(`Computer Score: ${computerScore}`);
         console.log(`Round: ${roundNumber}`);
-    }
 }
 
 
 const pSelection = 'scissors';
 const cSelection = computerPlay();
 
-playRound(pSelection, cSelection);
-game(pSelection, cSelection);
+//playRound(pSelection, cSelection);
+game();
