2e1da80eb7cdf9d0931de0f1c80b3f1f317ab36a
djballowe <djballowe@gmail.com>
2022-05-04 11:02:44 -0700

    added p h2 and div with js

--- Random Practice/htmlwithjs.js
+++ Random Practice/htmlwithjs.js
@@ -1,1 +1,23 @@
-alert('this is linked');
+const container = document.querySelector('#container');
+
+const red = document.createElement('p');
+red.classList.add('red');
+red.textContent = 'Hey Im red!';
+red.style.color = 'red';
+
+container.appendChild(red);
+
+const blue = document.createElement('h2');
+blue.classList.add('blue');
+blue.textContent = 'Hey Im blue!';
+blue.style.color = 'blue';
+
+container.appendChild(blue);
+
+const div = document.createElement('div');
+div.classList.add('content');
+div.style.border = 'thin solid black';
+div.style.backgroundColor = 'pink';
+
+container.appendChild(div);
+
