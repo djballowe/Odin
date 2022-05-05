e54f7ce2e50018de37f10c7469b0942180556779
djballowe <djballowe@gmail.com>
2022-05-05 08:34:41 -0700

    test commit on branch

--- Rock Paper Scissors/rps.js
+++ Rock Paper Scissors/rps.js
@@ -5,63 +5,68 @@ function game() {
 
         
         
-        const pSelection = userPrompt();
-        let cSelection = computerPlay();
+                const pSelection = 'paper';
+                let cSelection = computerPlay();
                 
-        function computerPlay() {
-                let choice = ['paper', 'rock', 'scissors'];
-                let ans = Math.floor(Math.random()*choice.length);
-                let cSelection = choice[ans];
-                return cSelection;
-        }
+                function computerPlay() {
+                        let choice = ['paper', 'rock', 'scissors'];
+                        let ans = Math.floor(Math.random()*choice.length);
+                        let cSelection = choice[ans];
+                        return cSelection;
+                }
 
-        const playRound = (pSelection, cSelection) => {
+                const playRound = (pSelection, cSelection) => {
                 
-                if (cSelection === 'paper' && pSelection === 'scissors') {
-                        roundNumber++;
-                        humanScore++;    
-                        return 'You Win! Scissors beats paper';
+                        if (cSelection === 'paper' && pSelection === 'scissors') {
+                                roundNumber++;
+                                humanScore++;    
+                                return 'You Win! Scissors beats paper';
                                 
-                }
-                if (cSelection === 'rock' && pSelection === 'scissors') {
-                        roundNumber++;
-                        computerScore++; 
-                        return 'You Lose! Rock beats scissors';
-                }
-                if (cSelection === 'scissors' && pSelection === 'scissors') {
-                        roundNumber++;
-                        return 'TIE';
-                }
-                if (cSelection === 'paper' && pSelection === 'rock') {
-                        roundNumber++;
-                        computerScore++;    
-                        return 'You Lose! Paper beats rock';
-                }
-                if (cSelection === 'rock' && pSelection === 'rock') {
-                        roundNumber++;
-                        return 'TIE';
-                }
-                if (cSelection === 'scissors' && pSelection === 'rock') {
-                        roundNumber++;
-                        humanScore++;    
-                        return 'You Win! Rock beats scissors';
-                } 
-                if (cSelection === 'paper' && pSelection === 'paper') {
+                        }
+                        if (cSelection === 'rock' && pSelection === 'scissors') {
+                                roundNumber++;
+                                computerScore++; 
+                                return 'You Lose! Rock beats scissors';
+                        }
+                        if (cSelection === 'scissors' && pSelection === 'scissors') {
+                                roundNumber++;
                                 return 'TIE';
-                }  
-                if (cSelection === 'rock' && pSelection === 'paper') {
-                        roundNumber++;
-                        humanScore++;    
-                        return 'You Win! Paper beats rock';
-                }
-                if (cSelection === 'scissors' && pSelection === 'paper') {
-                        roundNumber++;
-                        computerScore++;    
-                        return 'You Lose! Scissors beats paper';
-                }
+                        }
+                        if (cSelection === 'paper' && pSelection === 'rock') {
+                                roundNumber++;
+                                computerScore++;    
+                                return 'You Lose! Paper beats rock';
+                        }
+                        if (cSelection === 'rock' && pSelection === 'rock') {
+                                roundNumber++;
+                                return 'TIE';
+                        }
+                        if (cSelection === 'scissors' && pSelection === 'rock') {
+                                roundNumber++;
+                                humanScore++;    
+                                return 'You Win! Rock beats scissors';
+                        } 
+                        if (cSelection === 'paper' && pSelection === 'paper') {
+                                
+                                return 'TIE';
+                        }  
+                        if (cSelection === 'rock' && pSelection === 'paper') {
+                                roundNumber++;
+                                humanScore++;    
+                                return 'You Win! Paper beats rock';
+                        }
+                        if (cSelection === 'scissors' && pSelection === 'paper') {
+                                roundNumber++;
+                                computerScore++;    
+                                return 'You Lose! Scissors beats paper';
+                        }
                 
 
-        }
+                }
+                console.log(playRound(pSelection, cSelection));
+                console.log(`Human Score: ${humanScore}`);
+                console.log(`Computer Score: ${computerScore}`);
+                console.log(`Round: ${roundNumber}`);
         
 }
 
