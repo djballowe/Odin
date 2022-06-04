434ecb320a987bd24646ccb5ce3c26e540e976eb
djballowe <djballowe@gmail.com>
2022-06-04 12:27:30 -0700

    webpack practice

--- webpack-demo/webpack.config.js
+++ webpack-demo/webpack.config.js
@@ -3,6 +3,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 
 module.exports = {
+  mode: 'development',
   entry: './src/index.js',
   entry: {
 
@@ -11,11 +12,17 @@ module.exports = {
     print: './src/print.js',
 
   },
+  devtool: 'inline-source-map',
+  devServer: {
+
+    static: './dist',
+
+  },
   plugins: [
 
     new HtmlWebpackPlugin({
 
-      title: 'Output Management',
+      title: 'Development',
 
     }),
 
@@ -24,6 +31,11 @@ module.exports = {
     filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist'),
     clean: true,
+  },
+  optimization: {
+
+    runtimeChunk: 'single',
+
   },
 
 };
