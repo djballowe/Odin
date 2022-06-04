9a943a584c25040cfd8443ea13a6ce97d500c33b
djballowe <djballowe@gmail.com>
2022-06-04 11:43:23 -0700

    webpack practice

--- webpack-demo/src/index.js
+++ webpack-demo/src/index.js
@@ -1,28 +1,22 @@
 import _ from 'lodash';
-import './style.css';
-import Icon from './pain.jpg';
-import Data from './data.xml';
-import Notes from './data.csv';
+import printMe from './print.js';
+
 
 function component() {
     const element = document.createElement('div');
+    const btn = document.createElement('button');
   
-    // Lodash, now imported by this script
+    
     element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-    element.classList.add('hello');
 
-    // Add the image to our existing div.
+    btn.innerHTML = 'Click me and check the console!';
 
-  const myIcon = new Image();
-
-  myIcon.src = Icon;
+    btn.onclick = printMe;
 
 
-  element.appendChild(myIcon);
+    element.appendChild(btn);
+    
 
-  console.log(Data);
-  console.log(Notes);
-  
     return element;
   }
   
