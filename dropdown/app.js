4623c93be3fb8f36bde7a45fb11bf3d8dff3251f
djballowe <djballowe@gmail.com>
2022-06-21 13:54:56 -0700

    drop down

--- dropdown/app.js
+++ dropdown/app.js
@@ -1,1 +1,11 @@
-console.log('test');
+const drop = document.querySelector('#drop-down');
+const home = document.querySelector('#home');
+
+home.addEventListener('mouseover', () => {
+  drop.style.display = 'block';
+  window.addEventListener('mouseover', (e) => {
+    if (e.target.getAttribute('data') !== 'drop') {
+      drop.style.display = 'none';
+    }
+  });
+});
