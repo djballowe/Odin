2f1745e35134fd49a7620591e486c279682353dd
djballowe <djballowe@gmail.com>
2022-06-22 08:12:39 -0700

    mobile menu

--- Mobile Menu/app.js
+++ Mobile Menu/app.js
@@ -3,9 +3,11 @@ const menu = document.getElementById("pop-out");
 const burg = document.getElementById("hamburger");
 
 burg.addEventListener("click", () => {
-   menu.classList.add('pop-out-open');
+  menu.classList.remove('pop-out-close'); 
+  menu.classList.add('pop-out-open');
 });
 
 overlay.addEventListener('click', () => {
     menu.classList.remove('pop-out-open');
+    menu.classList.add('pop-out-close');
 })
