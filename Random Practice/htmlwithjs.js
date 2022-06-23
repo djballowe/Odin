d0e8af37193609be143dabe2a53a023a13ea3175
David Ballowe <djballowe@gmail.com>
2022-06-23 07:20:28 -0700

    test

--- Random Practice/htmlwithjs.js
+++ Random Practice/htmlwithjs.js
@@ -19,7 +19,7 @@ const blue = document.createElement('h2');
 blue.classList.add('blue');
 blue.textContent = 'Hey Im blue!';
 blue.style.color = 'blue';
-
+//change
 container.appendChild(blue);
 
 const div = document.createElement('div');
