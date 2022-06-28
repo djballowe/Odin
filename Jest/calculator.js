908d9214ddb246714ccb81eeabaf6985423dbd7e
David Ballowe <djballowe@gmail.com>
2022-06-27 19:12:14 -0700

    calculator

---
+++ Jest/calculator.js
@@ -0,0 +1,18 @@
+class Calculator {
+  add = () => {
+    let x = a + b;
+    return x;
+  };
+  subtract = () => {
+    let x = a - b;
+    return x;
+  };
+  divide = () => {
+    let x = a / b;
+    return x;
+  };
+  multioly = () => {
+    let x = a * b;
+    return x;
+  };
+}
