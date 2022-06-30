81f1575f8765ddbebf119756db3917e4bc3e02d8
djballowe <djballowe@gmail.com>
2022-06-30 09:59:24 -0700

    understanding how renders are handled in React

--- First React/hello-world/src/App.js
+++ First React/hello-world/src/App.js
@@ -3,12 +3,14 @@ import "./App.css";
 import { Greet } from "./components/Greet";
 import Welcome from "./components/Welcome"
 import Message from "./components/Message";
+import Counter from "./components/Counter"
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Message></Message>
+        <Counter></Counter>
+        {/* <Message></Message> */}
         {/* <Greet name="Bruce" heroName="Batman">
           <p>This is children props</p>
         </Greet>
